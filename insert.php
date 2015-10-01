7cb9493bf726044cf18ff4dd0e1d3c4a31b4d08d
BMigaliza <migaliza.lyza@gmail.com>
2015-10-01 00:02:29 +0000

    second commit

---
+++ insert.php
@@ -0,0 +1,20 @@
+<?php
+include("adb.php");
+$obj =new adb();
+
+	
+	//$new_date = date('yy-mm-dd',strtotime($_POST['Date']));
+	$reading=$_REQUEST['reading'];
+	$captured = $_REQUEST['captured'];
+	$date=$_REQUEST['date'];
+	$location=$_REQUEST['location'];
+	
+	$str_query="INSERT INTO 'phoneGapMetre' (Reading,Captured,Date,Location) VALUES('$reading','$captured','$date','$location')";
+
+	$obj->query($str_query) or  die(mysql_error());
+	//echo str-query;
+
+
+
+
+?>
